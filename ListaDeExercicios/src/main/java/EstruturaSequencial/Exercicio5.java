162b1555551aba9def645b51b8a3ccb894da5ffe
Bruno Pereira <dev.bruno.pereira@gmail.com>
2022-08-14 08:18:59 -0300

    Quinto Exercicio Concluido

---
+++ ListaDeExercicios/src/main/java/EstruturaSequencial/Exercicio5.java
@@ -0,0 +1,20 @@
+package EstruturaSequencial;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+    public static void main(String[] args) {
+        //Faça um Programa que converta metros para centímetros.
+        Scanner ler = new Scanner(System.in);
+        
+        float centimetro = 100;
+        float metro, resultado;
+        
+        System.out.print("Digite um valor em metro: ");
+        metro = ler.nextFloat();
+        
+        resultado = metro * centimetro;
+        System.out.println("Valor: "+ resultado +"cm");
+        ler.close();
+    }
+}
