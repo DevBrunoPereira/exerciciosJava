2e2b76d1f0869aae2617d5e1f7dfbca9279d2a61
Bruno Pereira <dev.bruno.pereira@gmail.com>
2022-08-15 12:56:59 -0300

    Exercicio 06 Cocluido

--- ListaDeExercicios/src/main/java/EstruturaSequencial/Exercicio06.java
+++ ListaDeExercicios/src/main/java/EstruturaSequencial/Exercicio06.java
@@ -5,9 +5,16 @@ import java.util.Scanner;
 public class Exercicio06 {
     public static void main(String[] args) {
         //Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+        //A = pi*raio*raio
+        float PI = 3.1415f;
+        float raio, area;
         Scanner ler = new Scanner(System.in);
         
-        System.out.print("Informe o valor do raio do círculo.");
+        System.out.print("Informe o valor do raio do círculo: ");
+        raio = ler.nextFloat();
+        area = (float) (PI * (Math.pow(raio, 2)));
+        
+        System.out.println("O valor da area do circulo é: " + area);
         
         ler.close();
     }
