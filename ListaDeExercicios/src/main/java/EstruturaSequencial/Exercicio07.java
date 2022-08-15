9e5ee55efbc37d1c5171b63795061db559dccfb7
Bruno Pereira <dev.bruno.pereira@gmail.com>
2022-08-15 13:02:34 -0300

    Iniciando o Exercicio 06

---
+++ ListaDeExercicios/src/main/java/EstruturaSequencial/Exercicio07.java
@@ -0,0 +1,17 @@
+package EstruturaSequencial;
+
+import java.util.Scanner;
+
+public class Exercicio07 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+        //Faça um Programa que calcule a área 
+        //de um quadrado, em seguida mostre o 
+        //dobro desta área para o usuário.
+        
+        
+        
+        ler.close();
+    }
+}
