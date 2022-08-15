aa70cc504a97b6e991c0816bf56d32cc56f0e158
Bruno Pereira <dev.bruno.pereira@gmail.com>
2022-08-15 20:56:10 -0300

    Iniciando exercicio 08

---
+++ ListaDeExercicios/src/main/java/EstruturaSequencial/Exercicio08.java
@@ -0,0 +1,17 @@
+package EstruturaSequencial;
+
+import java.util.Scanner;
+
+public class Exercicio08 {
+    public static void main(String[] args) {
+        //Faça um Programa que pergunte quanto 
+        //você ganha por hora e o número de horas 
+        //trabalhadas no mês. Calcule e mostre o 
+        //total do seu salário no referido mês.
+        Scanner sc = new Scanner(System.in);
+        
+        
+        
+        sc.close();
+    }
+}
