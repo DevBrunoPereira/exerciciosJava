172197d24902d4d1fdb1370b8b861b1d7f49aa49
Bruno Pereira <dev.bruno.pereira@gmail.com>
2022-08-15 12:41:00 -0300

    Iniciando o exercicio 06

--- ListaDeExercicios/src/main/java/EstruturaSequencial/Exercicio05.java
+++ ListaDeExercicios/src/main/java/EstruturaSequencial/Exercicio05.java
@@ -2,7 +2,7 @@ package EstruturaSequencial;
 
 import java.util.Scanner;
 
-public class Exercicio5 {
+public class Exercicio05 {
     public static void main(String[] args) {
         //Faça um Programa que converta metros para centímetros.
         Scanner ler = new Scanner(System.in);
---
+++ ListaDeExercicios/src/main/java/EstruturaSequencial/Exercicio06.java
@@ -0,0 +1,14 @@
+package EstruturaSequencial;
+
+import java.util.Scanner;
+
+public class Exercicio06 {
+    public static void main(String[] args) {
+        //Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+        Scanner ler = new Scanner(System.in);
+        
+        System.out.print("Informe o valor do raio do círculo.");
+        
+        ler.close();
+    }
+}
