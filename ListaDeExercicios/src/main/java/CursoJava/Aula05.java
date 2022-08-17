56ec94945d10bb1c243f8cdd165f8041b745ca3e
Bruno Pereira <dev.bruno.pereira@gmail.com>
2022-08-16 22:17:16 -0300

    Continuando o Exercicios

---
+++ ListaDeExercicios/src/main/java/CursoJava/Aula05.java
@@ -0,0 +1,6 @@
+package CursoJava;
+public class Aula05 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ListaDeExercicios/src/main/java/CursoJava/Aula06.java
@@ -0,0 +1,7 @@
+package CursoJava;
+public class Aula06 {
+    public static void main(String[] args) {
+        System.out.println("Você digitou " + args[0]);
+        
+    }
+}
