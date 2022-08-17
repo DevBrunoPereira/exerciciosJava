44fce471a9bace5fc50090963360cc5126b65d97
Bruno Pereira <dev.bruno.pereira@gmail.com>
2022-08-17 20:08:29 -0300

    exercicio 09 concluido

--- ListaDeExercicios/src/main/java/EstruturaSequencial/Exercicio09.java
+++ ListaDeExercicios/src/main/java/EstruturaSequencial/Exercicio09.java
@@ -1,11 +1,20 @@
 package EstruturaSequencial;
 
+import java.util.Scanner;
+
 public class Exercicio09 {
     public static void main(String[] args) {
         //Faça um Programa que peça a temperatura em graus Fahrenheit, 
         //transforme e mostre a temperatura em graus Celsius.
         //C = 5 * ((F-32) / 9).
+        Scanner sc = new Scanner(System.in);
+              
+        float celsius, fahrenheit;
+        System.out.print("Digite a temperatura em Fahrenheit: ");
+        fahrenheit = sc.nextFloat();
+        celsius = 5 * ( (fahrenheit - 32) / 9 );
         
-        
+        System.out.println("Celsius: "+ celsius +"C");
+        sc.close();
     }
 }
