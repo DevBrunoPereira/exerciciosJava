00d0c1b03c618517d8aa07775430cb27ff35b83f
Bruno Pereira <dev.bruno.pereira@gmail.com>
2022-08-18 20:47:31 -0300

    Iniciando o exercicio 10

---
+++ ListaDeExercicios/src/main/java/EstruturaSequencial/Exercicio10.java
@@ -0,0 +1,20 @@
+package EstruturaSequencial;
+
+import java.util.Scanner;
+
+/**
+ * Faça um Programa que peça a temperatura em 
+ * graus Celsius, transforme e mostre em graus
+ * Fahrenheit.
+ */
+public class Exercicio10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+        float celsius, fahrenheit;
+        
+        
+        sc.close();
+    }
+}
