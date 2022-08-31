3083cde96dfffd73c5837b95ab40d5d28ef42025
Bruno Pereira <dev.bruno.pereira@gmail.com>
2022-08-31 19:39:28 -0300

    Exercicio04

---
+++ ListaDeExercicios/src/main/java/EstrutudeDeDecisão/Exercicio04.java
@@ -0,0 +1,20 @@
+package EstrutudeDeDecisão;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    //Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String letra;
+        System.out.print("Digite uma letra: ");
+        letra = sc.next().toUpperCase();
+        System.out.println("A letra Digitada foi: " + letra);
+
+        if("A".equalsIgnoreCase(letra) || "e".equalsIgnoreCase(letra) || "i".equalsIgnoreCase(letra)){
+
+        }
+
+    }
+}
